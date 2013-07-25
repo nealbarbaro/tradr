298ae4b98700cdc7e60fe9649647fd8338cb814a
Neal Barbaro <nealbarbaro@gmail.com>
2013-07-24 23:27:03 -0700

    add foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tradr::Application.config.secret_token = '0e2244b81d3ef26ad15f0968b2835fe730e57207b874a2f7adf64991cba81c3a25ab0385dfa3a5433ee12f850b148bd3a3671e8ef7cbd686c19b90354b3e9a91'
